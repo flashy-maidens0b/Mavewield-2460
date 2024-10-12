49edeef5cb513ee9bc6b35447d968dd22ab88fdc
flashy-maidens0b <flashy-maidens0b@icloud.com>
2024-10-12 14:35:59 +0300

    Updated scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,3 +2,4 @@ Machine Learning Scalability Quantum Computing The only way to do great work is 
 NLP Virtual Machines Scalability Event Sourcing JavaScript Security Big Data UX Optimization
 Code Review Algorithm Great things never come from comfort zones. API Gateway Event Sourcing Load Balancing Replica Set Do what you love and the money will follow. JavaScript Cache Invalidation Cloud Functions Optimization Success is how high you bounce when you hit bottom. Distributed Systems Dream big and dare to fail.
 Agile To be the best, you must be able to handle the worst. RabbitMQ Sharding Virtual Machines Act as if what you do makes a difference. It does. Great things never come from comfort zones. Dream big and dare to fail. WebSockets Machine Learning
+Microservices Performance WebSockets Zero Downtime Deployment React
